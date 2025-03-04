a72effb2a949fa746dc3429a94a434ae40635f8b
florian.fogliani <florian.fogliani@epita.fr>
2025-03-04 10:49:12 +0100

    feat: converting wavenumber to wavelength

--- basic/extract.py
+++ basic/extract.py
@@ -1,5 +1,7 @@
 low_wave_number = 2000
 high_wave_number = 5000
+low_wave_length = 2
+high_wave_length = 5
 
 
 def atoi(str):
@@ -8,15 +10,19 @@ def atoi(str):
     except ValueError:
         raise valueError("Error in extracting data: string isn't a valid number.")
 
+def wave_number_to_wave_length(n):
+    return 1/n * 1e4
+
 def data_extract(path):
     file = open(path, "r")
     signal = {}
     for line in file:
         data = line.strip().split()
         wave_number = atoi(data[0])
+        wave_length = wave_number_to_wave_length(wave_number)
         absorbance = atoi(data[1])
-        if (wave_number >= low_wave_number and wave_number <= high_wave_number):
-            signal[wave_number] = absorbance
+        if (wave_length >= low_wave_length and wave_length <= high_wave_length):
+            signal[wave_length] = absorbance
     return signal
 
 
--- basic/plot.py
+++ basic/plot.py
@@ -1,11 +1,8 @@
 import matplotlib.pylab as plt
 
-low_wave_number = 2000
-high_wave_number = 5000
-
 def plot_sig(signal):
     x, y = zip(*(signal.items()))
     plt.plot(x,y)
     plt.ylabel("Absorbance (a.u.)")
-    plt.xlabel("Wavenumber (cm⁻¹)")
+    plt.xlabel("Wavelength (µm)")
     plt.show()
